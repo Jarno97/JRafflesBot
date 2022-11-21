8547f7dcfdae4a8c56e2bf81d5279c2f043838b0
Jarno97 <jarno_swinkels@live.nl>
2022-11-21 11:38:48 +0100

    update

--- autoUpdate.js
+++ autoUpdate.js
@@ -1,11 +1,10 @@
 const AutoGitUpdate = require('auto-git-update');
 
 const config = {
-    repository: 'https://github.com/chegele/BackupPurger',
-    fromReleases: true,
-    tempLocation: 'C:/Users/scheg/Desktop/tmp/',
-    ignoreFiles: ['util/config.js'],
-    executeOnComplete: 'C:/Users/scheg/Desktop/worksapce/AutoGitUpdate/startTest.bat',
+    repository: 'https://github.com/Jarno97/JRafflesBot',
+    fromReleases: false,
+    tempLocation: 'C:/temp/jraffles',
+    executeOnComplete: 'JRaffles.exe',
     exitOnComplete: true
 }
 
